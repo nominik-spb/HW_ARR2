ce69654ffdfc18513760526051eef254f16b8e84
nominik-spb <nominik.spb@gmail.com>
2025-03-26 18:14:27 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,79 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    public static String[] products = {"Хлеб", "Яблоки", "Молоко"};
-    public static int[] prices = {100, 200, 300};
-    public static int[] count = new int[products.length];
+    public static final int SIZE = 8;
+
+    public static void rotateColors(int[][] colors, int angleVar) {
+
+        // создаем пустой аналог первоначальной матрицы
+
+        int[][] rotatedColors = new int[SIZE][SIZE];
+
+        // заполняем аналог матрицы с выводом на экран в зависимости от переданного значения поворота
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (angleVar == 1) {
+                    rotatedColors[i][j] = colors[SIZE - 1 - j][i];
+                } else if (angleVar == 2) {
+                    rotatedColors[i][j] = colors[SIZE - 1 - i][SIZE - 1 - j];
+                } else {
+                    rotatedColors[i][j] = colors[j][SIZE - 1 - i];
+                }
+                System.out.format("%4d", rotatedColors[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
 
-        // вывод на экран информации о продуктах и стоимости
+        // создаем пустую матрицу
 
-        for (int i = 0; i < products.length; i++) {
-            System.out.println(i + 1 + ". " + products[i] + " " + prices[i] + " руб/шт");
+        int[][] colors = new int[SIZE][SIZE];
+
+        // заполняем матрицу случайными значениями
+
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                colors[i][j] = random.nextInt(256);
+            }
         }
-        int sumProducts = 0;
 
-        // сканируем ввод пользователя
+        // выводим матрицу на экран
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.format("%4d", colors[i][j]);
+            }
+            System.out.println();
+        }
+
+        // ждем ввода данных пользователем
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Выберите товар и количество (через пробел) или введите `end`");
+            System.out.println("Выберите угол поворота матрицы или введите `end`");
+            System.out.println("1. поворот на 90°");
+            System.out.println("2. поворот на 180°");
+            System.out.println("3. поворот на 270°");
             String input = scanner.nextLine();
 
             if ("end".equals(input)) {
                 break;
+            } else if (Integer.parseInt(input) > 3 || Integer.parseInt(input) < 1) {
+                System.out.println("введено некорректное значение\n");
+                continue;
             }
 
-            String[] parts = input.split(" ");
-            int prodNumber = Integer.parseInt(parts[0]) - 1;
-            int prodCount = Integer.parseInt(parts[1]);
+            // если введен корректный вариант поворота выполняем метод
 
-            count[prodNumber] = count[prodNumber] + prodCount;
+            rotateColors(colors, Integer.parseInt(input));
+            break;
         }
-
-        System.out.println("Ваша корзина:");
-        System.out.println("______________________________________");
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] > 0) {
-                sumProducts += prices[i] * count[i];
-                System.out.println(products[i] + " (" + count[i] + "шт. по " + prices[i] + " руб/шт.) в сумме - " + prices[i] * count[i] + " руб.");
-            }
-        }
-        System.out.println("______________________________________");
-        System.out.println("Итого: " + sumProducts + "руб.");
     }
 }
